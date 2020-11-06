e54cdd027a7d976b0756aa7f7e9ccde63742a23d
Sauban <s.sakhi19@hotmail.com>
2020-11-05 22:23:06 -0500

    adding login and dashboards screen

---
+++ app/src/main/java/com/saubantiago/personalmedicaldiary/activities/dashboard/Dashboard.java
@@ -0,0 +1,6 @@
+package com.saubantiago.personalmedicaldiary.activities.dashboard;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Dashboard extends AppCompatActivity {
+}
---
+++ app/src/main/java/com/saubantiago/personalmedicaldiary/activities/login/Login.java
@@ -0,0 +1,14 @@
+package com.saubantiago.personalmedicaldiary.activities.login;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.saubantiago.personalmedicaldiary.R;
+
+public class Login extends AppCompatActivity {
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
